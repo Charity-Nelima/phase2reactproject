f7bf92cc447ac870697dbb069a8f606a957b521f
Charity Nelima <wa.nelimacharity@gmail.com>
2022-10-07 17:30:08 +0300

    addition of app.js codes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom';
+import Header from './component/Header';
+import Search from './component/Search';
+import SearchResults from './component/SearchResults';
+import SavedSearches from './component/SavedSearches';
+import About from './component/About';
+import Contact from './component/Contact';
+import ClickMe from './component/ClickMe';
 
 function App() {
+  const [visibility, setVisibility] = useState(false);
+  const [searchData, setSearchData] = useState([]);
+  const [savedVisibility, setSavedVisibility] = useState(false);
+  const [saved, setSaved] = useState([]);
+  console.log(searchData);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='main'>
+      <Router>
+        <Route exact path='/'>
+          <Redirect to='/home' />
+        </Route>
+        <Header />
+        <Route exact path={'/contact'}>
+          <Contact className='contact' />
+        </Route>
+        <Route exact path={'/clickme'}>
+          <ClickMe />
+        </Route>
+        <Route exact path={'/about'}>
+          <About />
+        </Route>
+
+        <Route exact path={'/home'}>
+          <Search
+            setSearchData={setSearchData}
+            setVisibility={setVisibility}
+            setSavedVisibility={setSavedVisibility}
+            savedVisibility={savedVisibility}
+          />
+          {visibility ? (
+            <SearchResults
+              searchData={searchData}
+              saved={saved}
+              setSaved={setSaved}
+            />
+          ) : null}
+          {savedVisibility ? (
+            <SavedSearches saved={saved} setSaved={setSaved} />
+          ) : null}
+        </Route>
+      </Router>
     </div>
   );
 }
